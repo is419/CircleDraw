03e7fdf6de0efd55eae1c8060ee2c6e6aa9c9b5f
is419 <is419@ic.ac.uk>
2021-10-23 00:27:25 +0100

    tutorial 2

---
+++ src/main/java/Circle.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Circle {
+    private int rad;
+    private Point pos;
+    private Color col;
+
+    public Circle(Point initPos, Color col, int radius){
+        rad=radius;
+        pos=initPos;
+        this.col=col;
+
+    }
+
+    public void draw(Graphics g){
+        g.setColor(col);
+        g.fillOval(pos.x,pos.y,rad,rad);
+    }
+
+
+
+}
+
